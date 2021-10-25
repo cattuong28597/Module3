650fcd12d46def7dd25827236fa6cb37eecd92e3
cattuong28597 <cattuong28597@gmail.com>
2021-10-25 12:02:48 +0700

    lan1

--- quanlysinh vien/truyvan.sql
+++ quanlysinh vien/truyvan.sql
@@ -1,15 +1,36 @@
--- USE QuanLySinhVien;
--- -- SELECT *
--- -- FROM Student
--- -- WHERE Statuss = true;
+USE QuanLySinhVien;
+-- SELECT *
+-- FROM Student
+-- WHERE Statuss = true;
 
--- -- FROM Subjects
--- -- WHERE Credit < 10;
+-- FROM Subjects
+-- WHERE Credit < 10;
 
--- -- SELECT S.StudentId, S.studentName, C.ClassName
--- -- FROM Student S JOIN Class C ON S.ClassId = C.ClassID
--- -- WHERE C.ClassName = 'A1';
+-- SELECT S.StudentId, S.studentName, C.ClassName
+-- FROM Student S JOIN Class C ON S.ClassId = C.ClassID
+-- WHERE C.ClassName = 'A1';
 
 -- SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
 -- FROM Student S JOIN Mark M ON S.StudentId = M.StudentId JOIN Subjects Sub ON M.SubId = Sub.SubId
 -- WHERE Sub.SubName = 'CF';
+
+-- SELECT * FROM Student 
+-- WHERE Student.StudentName LIKE 'H%';
+
+-- SELECT * FROM Class
+-- WHERE Class.StartDate LIKE '%-12-%';
+
+-- SELECT * FROM Subjects
+-- WHERE Subjects.Credit BETWEEN 3 AND 5;
+
+-- UPDATE Student 
+-- SET ClassId = 2 WHERE StudentName = 'Hung';
+
+SELECT 
+	St.StudentName,
+    Sj.SubName,
+    M.Mark
+FROM Mark AS M
+INNER JOIN Student St ON M.StudentId = St.StudentId
+INNER JOIN Subjects Sj ON M.SubId = Sj.SubId    
+ORDER BY M.Mark DESC;
