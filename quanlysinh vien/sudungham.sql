5b0b37971e2e5c96245b32a2860e4258647f8f40
cattuong28597 <cattuong28597@gmail.com>
2021-10-25 10:37:21 +0700

    lan1

--- quanlysinh vien/sudungham.sql
+++ quanlysinh vien/sudungham.sql
@@ -7,9 +7,28 @@ USE QuanLySinhVien;
 --  SELECT S.StudentId, S.StudentName, AVG(Mark)
 --  FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
 --  GROUP BY S.StudentId, S.StudentName
--- HAVING AVG(Mark) > 15;
+-- HAVING AVG(Mark) > 9;
+
+-- SELECT S.StudentId, S.StudentName, AVG(Mark)
+-- FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
+-- GROUP BY S.StudentId, S.StudentName
+-- HAVING AVG(Mark) >= ALL ( SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
+
+-- SELECT SubId, SubName, Credit, Statuss
+-- FROM Subjects JOIN 
+-- (SELECT MAX(Credit) AS HighestCredit
+-- FROM Subjects) AS HC 
+-- ON Credit = HC.HighestCredit;
+
+-- SELECT SubId, SubName, Credit, Statuss
+-- FROM Subjects JOIN
+-- (SELECT SubId AS SI FROM Mark
+-- JOIN 
+-- (SELECT MAX(Mark) AS HighestMark FROM Mark) AS HM
+-- ON Mark.Mark = HM.HighestMark) AS SubMark
+-- ON Subjects.SubId = SubMark.SI;
 
 SELECT S.StudentId, S.StudentName, AVG(Mark)
-FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
-GROUP BY S.StudentId, S.StudentName
-HAVING AVG(Mark) >= ALL ( SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
+ FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
+ GROUP BY S.StudentId, S.StudentName
+ ORDER BY AVG(Mark) DESC;
--- quanlysinh vien/truyvan.sql
+++ quanlysinh vien/truyvan.sql
@@ -1,15 +1,15 @@
-USE QuanLySinhVien;
--- SELECT *
--- FROM Student
--- WHERE Statuss = true;
+-- USE QuanLySinhVien;
+-- -- SELECT *
+-- -- FROM Student
+-- -- WHERE Statuss = true;
 
--- FROM Subjects
--- WHERE Credit < 10;
+-- -- FROM Subjects
+-- -- WHERE Credit < 10;
 
--- SELECT S.StudentId, S.studentName, C.ClassName
--- FROM Student S JOIN Class C ON S.ClassId = C.ClassID
--- WHERE C.ClassName = 'A1';
+-- -- SELECT S.StudentId, S.studentName, C.ClassName
+-- -- FROM Student S JOIN Class C ON S.ClassId = C.ClassID
+-- -- WHERE C.ClassName = 'A1';
 
-SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
-FROM Student S JOIN Mark M ON S.StudentId = M.StudentId JOIN Subjects Sub ON M.SubId = Sub.SubId
-WHERE Sub.SubName = 'CF';
+-- SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+-- FROM Student S JOIN Mark M ON S.StudentId = M.StudentId JOIN Subjects Sub ON M.SubId = Sub.SubId
+-- WHERE Sub.SubName = 'CF';
